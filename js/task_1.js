b85f6da09716f08a37d84ddf4e162cdee01ea991
Alibek Beket <alibek1beket@gmail.com>
2023-04-07 03:03:20 +0600

    стилизовал блок уведомления

--- js/task_1.js
+++ js/task_1.js
@@ -2,11 +2,42 @@ const button = document.getElementById('add-item-btn');
 button.onclick = function () {
     const container = document.getElementById('container');
     const createDiv = document.createElement('div');
-    createDiv.innerText = 'Добавлен новый блок <div>!'
+    createDiv.innerText = 'Добавлен новый блок <div>!';
     const createButton = document.createElement('button');
-    createButton.innerText = 'Закрыть'
+    createButton.innerText = 'Закрыть';
     container.append(createDiv);
     createDiv.append(createButton);
+    container.style.position = 'relative';
+    createDiv.style = `
+        position: absolute;
+        right: 5px;
+        top: 5px;
+        width: 300px;
+        display: flex;
+        justify-content: space-between;
+        background-color: BlanchedAlmond;
+        border: solid 2px black;
+        padding: 5px 20px;
+        border-radius: 10px;
+    `;
+    createButton.style = `
+        border-radius: 10px;
+        background-color: Snow;
+    `;
+    createButton.onmouseover = function (event) {
+        createButton.style = `
+            background-color: black;
+            color: white;
+            border-radius: 10px;
+        `;
+    };
+    createButton.onmouseout = function (event) {
+        createButton.style = `
+            border-radius: 10px;
+            background-color: Snow;
+            color: black;
+        `;
+    };
     createButton.onclick = function () {
         createDiv.remove()
     };
